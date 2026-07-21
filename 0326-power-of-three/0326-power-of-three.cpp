f555d037650acb2be54f31efa4fbe40a6ebe2f4e
Kunal Singh <kunal.denexo@gmail.com>
2026-07-21 18:40:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -4,11 +4,10 @@ public:
         if (n <= 0) {
             return false;
         }
-        while ( n % 3 == 0) {
-            n/=3;
+        while (n % 3 == 0) {
+            n /= 3;
         }
-
-        return n==1;
-        
+        if (n == 1)
+            return true;
     }
 };
