0712603d6d3bb26e5879cc4cda71b8e8a9c16040
Kunal Singh <kunal.denexo@gmail.com>
2026-09-22 10:40:36 +0530

    Time: 0 ms (100%), Space: 9.7 MB (44.12%) - LeetHub

--- 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.cpp
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int theMaximumAchievableX(int num, int t) {
         int x = num + 2 * t;
-        if (num > 0) {
-            num++;
-        }
         return x;
     }
 };
